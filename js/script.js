eb5d969fd6da8b9830fb9b33a127eaae6978ed7f
doctorget <samuelsoares177778@gmail.com>
2020-05-10 15:05:54 -0300

    createPanel function is created

--- js/script.js
+++ js/script.js
@@ -5,20 +5,20 @@ let ctx = canvas.getContext('2d');
 let isWriting = false;
 
 let updateCanvas = () => {
-	canvas.height = window.innerHeight;
-	canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
 }
 
 let reset = () => {
     updateCanvas();
-	pixels = [];
-	createGrid();
+    pixels = [];
+    createGrid();
 }
 
 window.onload = updateCanvas();
 window.addEventListener('resize', e => reset());
 window.addEventListener('keyup', e => {
-    if(e.keyCode == 67) {
+    if (e.keyCode == 67) {
         reset();
     }
 });
@@ -32,25 +32,40 @@ let mouseY = 0;
 let pixels = [];
 
 class Pixel {
-	constructor(x, y, dim) {
-		this.x = x;
-		this.y = y;
-		this.isOn = false;
-		this.dim = dim;
-		this.bounding = [x, x + dim, y, y + dim];
+    constructor(x, y, dim) {
+        this.x = x;
+        this.y = y;
+        this.isOn = false;
+        this.dim = dim;
+        this.bounding = [x, x + dim, y, y + dim];
     }
     swipe() {
-		if (!this.isOn) {
-			ctx.fillStyle = '#0d0d0d';
-			ctx.strokeStyle = '#0d0d0d';
-		} else {
-			ctx.fillStyle = '#d3d3d3';
-			ctx.strokeStyle = '#d3d3d3'
-		}
-		ctx.beginPath();
-		ctx.fillRect(this.x, this.y, this.dim, this.dim);
-		ctx.lineWidth = 1;
-		ctx.strokeRect(this.x, this.y, this.dim, this.dim);
-	}
-    
+        if (!this.isOn) {
+            ctx.fillStyle = '#0d0d0d';
+            ctx.strokeStyle = '#0d0d0d';
+        } else {
+            ctx.fillStyle = '#d3d3d3';
+            ctx.strokeStyle = '#d3d3d3'
+        }
+        ctx.beginPath();
+        ctx.fillRect(this.x, this.y, this.dim, this.dim);
+        ctx.lineWidth = 1;
+        ctx.strokeRect(this.x, this.y, this.dim, this.dim);
+    }
 }
+
+let createPanel = () => {
+
+    pixelDim = 20;
+    anchorX = (canvas.width / 2) - (pixelDim * (gridWidth / 2));
+    anchorY = canvas.height / 2 - (pixelDim * (gridHeight / 5));
+
+    gridBounding = [anchorX, anchorX + (pixelDim * gridWidth), anchorY, anchorY + (pixelDim * gridHeight)];
+
+
+}
+
+let pixelDim = null;
+let anchorX = null;
+let anchorY = null;
+let gridBounding = null;
